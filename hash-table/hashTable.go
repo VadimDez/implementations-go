7813c0c0bbb31746ba3e273244dec99849759787
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-11-24 13:04:55 +0100

    hash table with no collision, no resize

--- hash-table/hashTable.go
+++ hash-table/hashTable.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math"
 	"fmt"
+	"errors"
 )
 
 type Item struct {
@@ -14,6 +15,7 @@ type Item struct {
 type HashTable struct {
 	array []Item
 	size int
+	count int
 }
 
 func (this HashTable) hash(k int, m int) int {
@@ -23,12 +25,50 @@ func (this HashTable) hash(k int, m int) int {
 func (this HashTable) exists(key int) bool {
 	hash := this.hash(key, this.size)
 
+	// no collisions for now
+	return !this.isNull(this.array[hash])
+}
+
+func (_ HashTable) isNull(item Item) bool {
+	return Item{} == item
+}
+
+func (this *HashTable) add(key int, value int) {
+	hash := this.hash(key, this.size)
+
+	this.array[hash] = Item{key, value, false}
+
+	this.count++
+}
+
+func (this HashTable) get(key int) (int, error) {
+	hash := this.hash(key, this.size)
+
+	if this.isNull(this.array[hash]) {
+		return 0, errors.New("Key is not present in hash table")
+	}
+
+	return this.array[hash].value, nil
+}
+
+func (this *HashTable) remove(key int) error {
+	hash := this.hash(key, this.size)
+
+	if this.isNull(this.array[hash]) {
+		return errors.New("Key is not present in hash table")
+	}
+
+	this.array[hash] = Item{}
+	this.count--
+
+	return nil
 }
 
 func NewHashTable() *HashTable {
 	table := HashTable{}
 
 	table.size = 8
+	table.count = 0
 	table.array = make([]Item, table.size)
 
 	return &table
@@ -38,4 +78,31 @@ func main() {
 	table := NewHashTable()
 
 	fmt.Println(table.hash(1, 8));
+
+	fmt.Println("Does key 1 exists: ", table.exists(1))
+
+	table.add(1, 100)
+
+	fmt.Println("Does key 1 exists now: ", table.exists(1))
+
+	fmt.Println("Get value at key: 1")
+
+	v, _ := table.get(1)
+
+	fmt.Println(v)
+
+
+	fmt.Println("Get value at key that doesn't exist yet: 2")
+
+	if _, e := table.get(2); e != nil {
+		fmt.Println("error: ", e)
+	}
+
+	fmt.Println("Remote item with key: 1")
+	if e := table.remove(1); e == nil {
+		fmt.Println("Removed")
+	} else {
+		fmt.Println("Error during removing: ", e)
+	}
+	fmt.Println("Does key 1 still exists: ", table.exists(1))
 }
