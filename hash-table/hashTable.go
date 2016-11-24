44b8cfa339f97c38534eb516d4c8a1ada6a20cad
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-11-24 21:06:34 +0100

    hash table, amortised, rewrite on duplicate

--- hash-table/hashTable.go
+++ hash-table/hashTable.go
@@ -9,7 +9,7 @@ import (
 type Item struct {
 	key int
 	value int
-	deleted bool
+	isDeleted bool
 }
 
 type HashTable struct {
@@ -24,44 +24,136 @@ func (this HashTable) hash(k int, m int) int {
 
 func (this HashTable) exists(key int) bool {
 	hash := this.hash(key, this.size)
+	found := false
+	end := false
+	i := 0
 
-	// no collisions for now
-	return !this.isNull(this.array[hash])
+	for i < this.size && !found && !end {
+		position := (hash + i) % this.size // circular
+
+		if this.isNull(this.array[position]) { // do not proceed
+			end = true
+		} else {
+			if !this.array[position].isDeleted && this.array[position].key == hash {
+				found = true
+			}
+
+			i++
+		}
+	}
+
+	return found
 }
 
 func (_ HashTable) isNull(item Item) bool {
 	return Item{} == item
 }
 
-func (this *HashTable) add(key int, value int) {
+func (this *HashTable) add(key int, value int) error {
 	hash := this.hash(key, this.size)
+	added := false
+	i := 0
 
-	this.array[hash] = Item{key, value, false}
+	for i < this.size && !added {
+		position := (hash + i) % this.size // make it circular
 
-	this.count++
+		if this.isNull(this.array[position]) || this.array[position].isDeleted {
+			this.array[position] = Item{key, value, false}
+			this.count++
+			added = true
+		} else if this.array[position].key == key {
+			this.array[position].value = value
+			added = true
+		}
+
+		i++
+	}
+
+	if !added {
+		return errors.New("Error: item wasn't added")
+	}
+
+	if this.count >= this.size * 3/4 {
+		this.resize(this.size * 2)
+	}
+
+	return nil
 }
+
 
 func (this HashTable) get(key int) (int, error) {
 	hash := this.hash(key, this.size)
+	found, position := this.findWith(hash, key)
 
-	if this.isNull(this.array[hash]) {
+	if !found {
 		return 0, errors.New("Key is not present in hash table")
 	}
 
-	return this.array[hash].value, nil
+	return this.array[position].value, nil
 }
 
 func (this *HashTable) remove(key int) error {
 	hash := this.hash(key, this.size)
+	found, position := this.findWith(hash, key)
 
-	if this.isNull(this.array[hash]) {
+	if !found {
 		return errors.New("Key is not present in hash table")
 	}
 
-	this.array[hash] = Item{}
+	this.array[position].isDeleted = true
+	this.array[position].value = -1
+	this.array[position].key = -1
+
 	this.count--
 
+	if this.count <= this.size / 2 {
+		this.resize(this.size / 2)
+	}
+
 	return nil
+}
+
+// resize and rehash all items
+func (this *HashTable) resize(size int) {
+	newTable := HashTable{}
+	newTable.size = size
+	newTable.count = 0
+	newTable.array = make([]Item, size)
+
+	for i := 0; i < this.size; i++ {
+		item := this.array[i]
+
+		if !this.isNull(item) && !item.isDeleted {
+			newTable.add(item.key, item.value)
+		}
+	}
+
+	this.array = newTable.array
+	this.size = newTable.size
+}
+
+// Get position of item by key if found
+func (this HashTable) findWith(hash int, key int) (bool, int) {
+	i := 0
+	found := false
+	end := false
+	var position int
+
+	for i < this.size && !found && !end {
+		position = (hash + i) % this.size
+
+		if this.isNull(this.array[position]) {
+			end = true
+		} else {
+			if !this.array[position].isDeleted && this.array[position].key == key {
+				found = true
+			}
+		}
+
+		i++
+	}
+
+	return found, position
 }
 
 func NewHashTable() *HashTable {
@@ -77,11 +169,13 @@ func NewHashTable() *HashTable {
 func main() {
 	table := NewHashTable()
 
-	fmt.Println(table.hash(1, 8));
+	fmt.Println("hash of k=1, m=8: ", table.hash(1, 8));
 
 	fmt.Println("Does key 1 exists: ", table.exists(1))
 
-	table.add(1, 100)
+	if e := table.add(1, 100); e != nil {
+		fmt.Println("add error: ", e)
+	}
 
 	fmt.Println("Does key 1 exists now: ", table.exists(1))
 
@@ -105,4 +199,33 @@ func main() {
 		fmt.Println("Error during removing: ", e)
 	}
 	fmt.Println("Does key 1 still exists: ", table.exists(1))
+
+	if e := table.add(1, 1); e != nil {
+		fmt.Println("add error: ", e)
+	}
+	if e := table.add(9, 9); e != nil {
+		fmt.Println("add error: ", e)
+	}
+
+	table.add(9, 9)
+	table.add(1, 9)
+	table.add(2, 9)
+	table.add(3, 9)
+	table.add(4, 9)
+	table.add(5, 9)
+	table.add(6, 9)
+	table.add(7, 9)
+	table.add(8, 9)
+	if err := table.add(10, 9); err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+
+
+	v, e := table.get(1)
+	fmt.Println("Get value at key: 1 :", v, "Error: ", e)
+	v, e = table.get(9)
+	fmt.Println("Get value at key: 9 :", v, "Error: ", e)
+	fmt.Println("length: ", table.count)
+	fmt.Println("length: ", table.size)
 }
